3fa860e8fa5cf420db9253c0a0aa36522b0f0722
SAM LLOYD <SL295211@falmouth.ac.uk>
2026-01-22 14:50:50 +0000

    started oon authetication

--- GMTK 2025 Dog loop/Assets/Menus/SceneLoader.cs
+++ GMTK 2025 Dog loop/Assets/Menus/SceneLoader.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
 
-    public void LoadScene(string Name) 
+    public async void LoadScene(string Name) 
     {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
         SceneManager.LoadScene(Name);
     }
 
--- GMTK 2025 Dog loop/Assets/Scoring/ScoreManager.cs
+++ GMTK 2025 Dog loop/Assets/Scoring/ScoreManager.cs
@@ -1,6 +1,8 @@
 using System;
+using Unity.Services.Leaderboards;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -9,20 +11,21 @@ public class ScoreManager : MonoBehaviour
     [SerializeField]
     private FloatStore TotalScore;
 
+    [SerializeField]
+    private string LeaderBoardID;
+
     private void Start()
     {
         TotalScore.SetValue(0f);
     }
 
-    private void OnDestroy()
+    private async void OnDestroy()
     {
         foreach (RegisterFurniture furn in RemainingFurniture.GetItems())
         {
             TotalScore.SetValue(furn.furniture.GetScore() + TotalScore.GetValue());
         }
-        if (TotalScore.GetValue() > PlayerPrefs.GetFloat("PersonalHighScore"))
-        {
-            PlayerPrefs.SetFloat("PersonalHighScore", TotalScore.GetValue());
-        }
+
+        await LeaderboardsService.Instance.AddPlayerScoreAsync(LeaderBoardID, TotalScore.GetValue());
     }
 }
